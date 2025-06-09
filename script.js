e28ca67105cdd396bee99a0463663add590f5d21
ThamilyBravo <thamilyrsbravo17@gmail.com>
2025-06-09 11:26:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// TRANSFORMAR MENU HAMBURGUER EM X
+
+const menuHamburguer = document.querySelector ('.menu-hamburguer');
+
+menuHamburguer .addEventListener ('click', () => {
+    toggleMenu ();
+});
+
